d69c2c66b35421ebae278fbe3f700366e9087979
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-10-03 15:47:07 -0600

    Setup data reader for test.

--- tests/load_data_test.py
+++ tests/load_data_test.py
@@ -1,6 +1,6 @@
 from fmbase.source.merra2.local.reader import MERRADataProcessor
 from fmbase.util.config import configure
-configure( 'explore-test1', "../config" )
+configure( 'explore-test1' )
 
 reader = MERRADataProcessor()
 reader.process( 'inst3_3d_asm_Np', )
