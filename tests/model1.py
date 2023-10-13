2a75da27e8518ecf32f233188a380df91dca7a1c
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-10-13 13:42:44 -0600

    create MERRA2 DataInterface

--- tests/model1.py
+++ tests/model1.py
@@ -1,7 +1,8 @@
 from fmbase.source.merra2.model import MERRA2DataInterface
 from fmbase.util.config import configure
+import xarray as xa
 configure( 'explore-test1' )
 
 datasetMgr = MERRA2DataInterface()
-datasetMgr.load_timestep()
+tsdata: xa.DataArray = datasetMgr.load_timestep( 2000, 0 )
 
