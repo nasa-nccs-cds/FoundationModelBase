b375f102826bf2ae6164b4a1382652d5d3df1424
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-08 13:04:01 -0700

    Update preprocessing

--- tests/preprocess.py
+++ tests/preprocess.py
@@ -2,7 +2,7 @@ from fmbase.source.merra2.local.preprocess import MERRA2DataProcessor
 from fmbase.util.config import configure
 import hydra, xarray as xa
 hydra.initialize( version_base=None, config_path="../config" )
-configure( 'explore-test1' )
+configure( 'explore-exp1' )
 
 reader = MERRA2DataProcessor()
-reader.process( reprocess=True )
+reader.process( reprocess=False )
