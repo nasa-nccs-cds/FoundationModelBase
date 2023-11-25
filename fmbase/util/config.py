545e471f3eb607494c8c6d86a690e5dd11ad3102
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-25 15:43:05 -0700

    Update preprocessing

--- fmbase/util/config.py
+++ fmbase/util/config.py
@@ -1,33 +1,24 @@
 from omegaconf import DictConfig, OmegaConf
-import sys
+import sys, hydra
 
 def cfg() -> DictConfig:
     return Configuration.instance().cfg
 
-def configure(config_name: str, config_path: str = None):
-    Configuration.init( config_name, config_path )
+def configure(config_name: str):
+    Configuration.init( config_name )
 
 class Configuration:
     _instance = None
     _instantiated = None
 
-    def __init__(self, config_name: str, config_path: str ):
+    def __init__(self, config_name: str ):
         self.config_name = config_name
-        self.config_path = config_path
-        self.cfg: DictConfig = self.get_config()
-
-    def get_config(self) -> DictConfig:
-        if 'hydra' in sys.modules:
-            import hydra
-            dcfg: DictConfig = hydra.compose( self.config_name, return_hydra_config=True )
-        else:
-            dcfg: DictConfig = OmegaConf.load(f"{self.config_path}/{self.config_name}.yaml")
-        return dcfg
+        self.cfg: DictConfig = hydra.compose( self.config_name, return_hydra_config=True )
 
     @classmethod
-    def init(cls, config_name: str, config_path: str ):
+    def init(cls, config_name: str ):
         if cls._instance is None:
-            inst = cls( config_name, config_path )
+            inst = cls( config_name )
             cls._instance = inst
             cls._instantiated = cls
 
