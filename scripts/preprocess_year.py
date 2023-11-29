b5352860c4abd82dc82f45241dfbda30b6d83c5f
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-28 23:10:19 -0700

    Update preprocessing

--- scripts/preprocess_year.py
+++ scripts/preprocess_year.py
@@ -9,13 +9,13 @@ hydra.initialize( version_base=None, config_path="../config" )
 configure( 'merra2-test' )
 
 nproc = round(cpu_count()*0.9)
-years = list( range( 1986, 2021 ) )
+years = list( range( 1984, 2022 ) )
 months = list(range(0,12,1))
 month_years = [ (month,year) for year in years for month in months  ]
 
 def process( month_year: Tuple[int,int] ) -> StatsAccumulator:
 	reader = MERRA2DataProcessor()
-	return reader.process_year( month_year[1], month=month_year[0], reprocess=True )
+	return reader.process_year( month_year[1], month=month_year[0], reprocess=False )
 
 if __name__ == '__main__':
 	print( f"Multiprocessing {len(month_years)} months with {nproc} procs")
