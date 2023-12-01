961251702dd12d62ad7470632a9fe1c76fcd6b66
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-01 13:34:19 -0700

    Update preprocessing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,25 +1,26 @@
 from fmbase.source.merra2.preprocess import MERRA2DataProcessor, StatsAccumulator
 from fmbase.util.config import configure
-from typing import List
+from typing import List, Tuple
 from multiprocessing import Pool, cpu_count
-from fmbase.util.config import cfg
 import hydra
 
 hydra.initialize( version_base=None, config_path="../config" )
-configure( 'merra2-test' )
+configure( 'merra2-finetuning' )
 
-year_range = cfg().preprocess.year_range
-years = list(range(*year_range))
-nproc = round(cpu_count()*0.9)
+nproc = cpu_count()-2
+years = list( range( 1984, 2022 ) )
+months = list(range(0,12,1))
+month_years = [ (month,year) for year in years for month in months  ]
 
-def process( year: int ) -> StatsAccumulator:
+def process( month_year: Tuple[int,int] ) -> StatsAccumulator:
 	reader = MERRA2DataProcessor()
-	return reader.process_month( year, reprocess=True)
+	return reader.process_month( month_year[1], month_year[0], reprocess=False)
 
 if __name__ == '__main__':
-	print( f"Multiprocessing {len(years)} years with {nproc} procs")
+	print( f"Multiprocessing {len(month_years)} months with {nproc} procs")
 	with Pool(processes=nproc) as pool:
-		proc_stats: List[StatsAccumulator] = pool.map( process, years )
+		proc_stats: List[StatsAccumulator] = pool.map( process, month_years )
 		MERRA2DataProcessor().save_stats(proc_stats)
+
 
 
