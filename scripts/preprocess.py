ae1f3f799640879b3f8bdcdc23e0c800ca58f1c8
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-01 15:53:08 -0700

    Update preprocessing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -8,8 +8,8 @@ hydra.initialize( version_base=None, config_path="../config" )
 configure( 'merra2-finetuning' )
 
 nproc = cpu_count()-2
-years = list( range( 1984, 1986 ) )
-months = list(range(0,12,1))
+years = list( range( 1984, 1985 ) )
+months = list(range(0,6,1))
 month_years = [ (month,year) for year in years for month in months  ]
 
 def process( month_year: Tuple[int,int] ) -> StatsAccumulator:
