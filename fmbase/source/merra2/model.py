99313ba5baf6db20a8e5e672605185d152b485ff
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-04 12:43:39 -0700

    Update preprocessing

--- fmbase/source/merra2/model.py
+++ fmbase/source/merra2/model.py
@@ -76,20 +76,21 @@ def load_timestep( year: int, month: int, day: int, task: Dict, **kwargs ) -> xa
 					level_arrays = []
 					for iL, lev in enumerate(levs):
 						level_array: xa.DataArray = varray.sel( **{zc:lev}, method="nearest", drop=False )
-						level_array = replace_nans( level_array, cmap )
+						level_array = replace_nans( level_array, cmap['y'] )
 						level_arrays.append( level_array )
-					varray = xa.concat( level_arrays, zc ).transpose(*corder, missing_dims="ignore")
+					varray = xa.concat( level_arrays, cmap ).transpose(*corder, missing_dims="ignore")
 				varray.attrs['dset_name'] = dsname
 				print( f" >> Load_var({dsname}): name={vname}, shape={varray.shape}, dims={varray.dims}, zc={zc}, mean={varray.values.mean()}, nnan={nnan(varray)} ({pctnan(varray)})")
 				tsdata[vname] = varray
 	return xa.Dataset( tsdata )
 
 def replace_nans( level_array: xa.DataArray, cmap: Dict[str,str] ) -> xa.DataArray:
-	if nnan(level_array):
-		level_array =  level_array.interpolate_na( dim=cmap['x'], method="linear", fill_value="extrapolate" )
-		if nnan(level_array):
-			level_array = level_array.interpolate_na(dim=cmap['y'], method="linear", fill_value="extrapolate" )
-		assert nnan(level_array) == 0, "Error, NaNs remaining in input data after interpolate_na"
+	if nnan(level_array) > 0:
+		result: xa.DataArray =  level_array.interpolate_na( dim=cmap['x'], method="linear", fill_value="extrapolate", keep_attrs=True )
+		if nnan(result) > 0:
+			result: xa.DataArray = level_array.interpolate_na( dim=cmap['y'], method="linear", fill_value="extrapolate", keep_attrs=True )
+		assert nnan(result) == 0, "Error, NaNs remaining in input data after interpolate_na"
+		return result
 	return level_array
 
 def load_batch( year: int, month: int, day: int, ndays: int, task_config: Dict, **kwargs ) -> xa.Dataset:
