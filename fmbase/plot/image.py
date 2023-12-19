e3981211493e53a7df84279bde2c5031bc93c375
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-19 14:02:04 -0700

    Update preprocessing

--- fmbase/plot/image.py
+++ fmbase/plot/image.py
@@ -1,11 +1,17 @@
 import hvplot.xarray  # noqa
 import xarray as xa
 from fmbase.util.ops import xaformat_timedeltas
+import panel.widgets as pnw
+
+
 def plot( ds: xa.Dataset, vname: str, **kwargs ):
-	ds.assign_coords( time=xaformat_timedeltas(ds.coords['time']) )
+	time: xa.DataArray = ds.coords['time']
+	ds.assign_coords( time=xaformat_timedeltas(time) )
 	dvar: xa.DataArray = ds.data_vars[vname]
 	x = kwargs.get( 'x', 'lon' )
 	y = kwargs.get( 'y', 'lat')
 	z = kwargs.get( 'z', 'level')
 	groupby = [ d for d in ['time',z] if d in dvar.coords ]
-	return dvar.hvplot.image(x=x, y=y, groupby=groupby, cmap='jet', title=vname )
+
+	time = pnw.Player(name='time', start=0, end=time.size, loop_policy='loop', interval=100)
+	return dvar.interactive(loc='bottom').isel(time=time).image( x=x, y=y, groupby=groupby, cmap='jet', title=vname )
