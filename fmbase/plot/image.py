17b75057cee4c212bb8d3007137189171acc2f10
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-19 14:48:19 -0700

    Update preprocessing

--- fmbase/plot/image.py
+++ fmbase/plot/image.py
@@ -2,6 +2,7 @@ import hvplot.xarray  # noqa
 import xarray as xa
 from fmbase.util.ops import xaformat_timedeltas
 import panel as pn
+import ipywidgets as ipw
 
 
 def plot( ds: xa.Dataset, vname: str, **kwargs ):
@@ -13,6 +14,8 @@ def plot( ds: xa.Dataset, vname: str, **kwargs ):
 	z = kwargs.get( 'z', 'level')
 	print( f"Plotting {vname}{dvar.dims}, shape = {dvar.shape}")
 #	tslider = pnw.DiscreteSlider( name='time', options =time.values.tolist() )
-	tslider = pn.widgets.DiscreteSlider( name='time', options =list(range(time.size)) )
-	figure = ( dvar.interactive(loc='bottom').isel(time=tslider).hvplot( cmap='jet', x="lon", y="lat", data_aspect=1 ) )   #.image( x=x, y=y, groupby=groupby, cmap='jet', title=vname )
-	return pn.Column( f"# {vname}", figure ).servable()
+#	tslider = pn.widgets.DiscreteSlider( name='time', options =list(range(time.size)) )
+	tslider = ipw.IntSlider(description='time', min=0, max=time.size-1)
+	return dvar.interactive(loc='bottom').isel(time=tslider).hvplot( cmap='jet', x="lon", y="lat", data_aspect=1 )
+	#figure = ( dvar.interactive(loc='bottom').isel(time=tslider).hvplot( cmap='jet', x="lon", y="lat", data_aspect=1 ) )   #.image( x=x, y=y, groupby=groupby, cmap='jet', title=vname )
+	#return pn.Column( f"# {vname}", figure ).servable()
