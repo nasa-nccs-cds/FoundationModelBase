f7a0a975a068efe80dd189b6ade96aff198dff85
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-20 20:01:19 -0700

    Update preprocessing

--- fmbase/plot/image.py
+++ fmbase/plot/image.py
@@ -35,13 +35,14 @@ def plot1( ds: xa.Dataset, vname: str, **kwargs ):
 
 def mplplot( target: xa.Dataset, forecast: xa.Dataset, vnames: List[str], **kwargs):
 	time: xa.DataArray = xaformat_timedeltas( target.coords['time'] )
+	levels: xa.DataArray = target.coords['level']
 	target.assign_coords( time=time )
 	forecast.assign_coords(time=time)
 	ptypes = ['target', 'forecast', 'difference']
 	ims, pvars, nvars = {}, {}, len(vnames)
-	plt.rcParams['figure.figsize'] = [nvars*4, 8]
+	lslider: ipw.IntSlider = ipw.IntSlider( value=0, min=0, max=levels.size-1, description='Level Index:', )
 	with plt.ioff():
-		fig, axs = plt.subplots(nrows=nvars, ncols=3, sharex=True, sharey=True, figsize=[nvars*4, 8], layout="tight")
+		fig, axs = plt.subplots(nrows=nvars, ncols=3, sharex=True, sharey=True, figsize=[nvars*3, 15], layout="tight")
 	for iv, vname in enumerate(vnames):
 		tvar: xa.DataArray = target.data_vars[vname].squeeze(dim="batch", drop=True)
 		fvar: xa.DataArray = forecast.data_vars[vname].squeeze(dim="batch", drop=True)
@@ -49,7 +50,10 @@ def mplplot( target: xa.Dataset, forecast: xa.Dataset, vnames: List[str], **kwar
 		for it, pvar in enumerate( [tvar,fvar,diff] ):
 			ax = axs[ iv, it ]
 			ax.set_aspect(0.5)
-			ims[(iv,it)] =  pvar.isel(time=0).plot.imshow( ax=ax, x="lon", y="lat", cmap='jet', yincrease=True )
+			tslice: xa.DataArray = pvar.isel(time=0)
+			if "level" in tslice.dims:
+				tslice = tslice.isel(level=lslider.value)
+			ims[(iv,it)] =  tslice.plot.imshow( ax=ax, x="lon", y="lat", cmap='jet', yincrease=True )
 			pvars[(iv,it)] =  pvar
 
 	def update(change):
@@ -60,9 +64,12 @@ def mplplot( target: xa.Dataset, forecast: xa.Dataset, vnames: List[str], **kwar
 				ax1 = axs[iv1, it1]
 				ax1.set_title( f"{vname1} {ptypes[it1]}: {tval}")
 				im1, dvar1 = ims[ (iv1, it1) ], pvars[ (iv1, it1) ]
-				im1.set_data( dvar1.isel(time=sindex).values )
+				tslice1: xa.DataArray =  dvar1.isel(time=sindex)
+				if "level" in tslice1.dims:
+					tslice1 = tslice1.isel(level=lslider.value)
+				im1.set_data( tslice1.values )
 				fig.canvas.draw_idle()
 
-	slider = ipw.IntSlider( value=0, min=0, max=time.size-1 )
-	slider.observe(update, names='value')
-	return ipw.VBox([slider, fig.canvas])
+	tslider = ipw.IntSlider( value=0, min=0, max=time.size-1, description='Time Index:', )
+	tslider.observe(update, names='value')
+	return ipw.VBox([tslider, lslider, fig.canvas])
