6034a0a2472fa8accdcf74ad5f7c4c3c5646c54b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-21 16:52:48 -0700

    Update preprocessing

--- fmbase/plot/image.py
+++ fmbase/plot/image.py
@@ -56,37 +56,40 @@ def mplplot( target: xa.Dataset, forecast: xa.Dataset, vnames: List[str], **kwar
 		tvar: xa.DataArray = target.data_vars[vname].squeeze(dim="batch", drop=True)
 		fvar: xa.DataArray = forecast.data_vars[vname].squeeze(dim="batch", drop=True)
 		diff: xa.DataArray = tvar - fvar
+		vrange = None
 		for it, pvar in enumerate( [tvar,fvar,diff] ):
 			ax = axs[ iv, it ]
 			ax.set_aspect(0.5)
-			ax.set_title(f"{vname} {ptypes[it]}")
-			vmin, vmax = cscale( pvar, 2.0 )
+			if it != 1: vrange = cscale( pvar, 2.0 )
 			tslice: xa.DataArray = pvar.isel(time=tslider.value)
 			if "level" in tslice.dims:
 				tslice = tslice.isel(level=lslider.value)
-			ims[(iv,it)] =  tslice.plot.imshow( ax=ax, x="lon", y="lat", cmap='jet', yincrease=True, vmin=vmin, vmax=vmax  )
+			ims[(iv,it)] =  tslice.plot.imshow( ax=ax, x="lon", y="lat", cmap='jet', yincrease=True, vmin=vrange[0], vmax=vrange[1]  )
 			pvars[(iv,it)] =  pvar
+			ax.set_title(f"{vname} {ptypes[it]}")
 
 	def time_update(change):
 		sindex = change['new']
-		print( f"time index: {sindex}" )
 		for iv1, vname1 in enumerate(vnames):
 			for it1 in range(3):
+				ax1 = axs[iv1, it1]
 				im1, dvar1 = ims[ (iv1, it1) ], pvars[ (iv1, it1) ]
 				skw = dict(level=lslider.value, time=sindex) if "level" in dvar1.dims else dict(time=sindex)
 				tslice1: xa.DataArray =  dvar1.isel(**skw)
 				im1.set_data( tslice1.values )
+				ax1.set_title(f"{vname} {ptypes[it]}")
 				fig.canvas.draw_idle()
 
 	def level_update(change):
 		lindex = change['new']
-		print( f"level index: {lindex}" )
 		for iv1, vname1 in enumerate(vnames):
 			for it1 in range(3):
+				ax1 = axs[iv1, it1]
 				im1, dvar1 = ims[ (iv1, it1) ], pvars[ (iv1, it1) ]
 				skw = dict(level=lindex,time=tslider.value) if "level" in dvar1.dims else dict(time=tslider.value)
 				tslice1: xa.DataArray =  dvar1.isel(**skw)
 				im1.set_data( tslice1.values )
+				ax1.set_title(f"{vname} {ptypes[it]}")
 				fig.canvas.draw_idle()
 
 	tslider.observe( time_update,  names='value' )
